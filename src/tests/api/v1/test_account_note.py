ec3442b2fd8a150be59dd9f4c4c00a2d85f8c019
chrisorz <imchrisorz@gmail.com>
2022-11-23 23:04:44 +0800

    test: 新增单元测试

--- src/tests/api/v1/test_account_note.py
+++ src/tests/api/v1/test_account_note.py
@@ -5,8 +5,35 @@ from src.config.config import settings
 
 client = TestClient(app)
 
+BASE_URL = settings.API_V1_STR
+
 
 def test_app():
-    r = client.get(f'{settings.API_V1_STR}/account_note')
+    r = client.get(f'{BASE_URL}')
     response = r.json()
-    assert type(response) == list
+    assert response['code'] == 0
+
+
+# def test_get_account_types():
+#     r = client.get(f'{BASE_URL}/account_type')
+#     response = r.json()
+#     print('response:', response)
+#     assert response['code'] == 0
+#     assert type(response['data']) == list
+#
+#
+# def test_create_account_type():
+#     data = {"amount_type": "0", "type_zh_name": "服装", "type_en_name": "clothing"}
+#     r = client.post(
+#         f'{BASE_URL}/account_type',
+#         headers={'Content-Type': 'application/json'},
+#         json=data
+#     )
+#     response = r.json()
+#
+#     print(response)
+#
+#     assert response['code'] == 0
+#     assert response['data']['type_zh_name'] == data['type_zh_name']
+#     assert response['data']['type_en_name'] == data['type_en_name']
+#     assert response['data']['amount_type'] == data['amount_type']
